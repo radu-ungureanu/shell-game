7cb860ee7243c287e4a8a2786977ba7b5ac8ed46
Radu Ungureanu <radu.ungureanu@outlook.com>
2014-11-09 13:25:11 +0200

    dasdas

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var cups = [];
 var ball;
 var ballPosition;
 var layer;
+var noOfSwapsMade = 0;
+var maxNoOfSwaps = 10;
 
 $(document).ready(function () {
     var stage = new Kinetic.Stage({
@@ -11,21 +13,34 @@ $(document).ready(function () {
     });
     layer = new Kinetic.Layer();
 
+    var border = new Kinetic.Rect({
+        width: stage.getWidth(),
+        height: stage.getHeight(),
+        stroke: 'black',
+        strokeWidth: 4, //Border Size in Pixels
+        fill: '#00FF00' //Background Color
+    });
+    layer.add(border);
+
     // create objects
     for (var i = 0; i < 3; i++) {
         var cup = new Kinetic.Rect({
-            x: 200 * i,
-            y: 200,
+            x: 50 + 200 * i,
+            y: 150,
             width: 100,
             height: 50,
             fill: '#00D2FF',
             stroke: 'black',
             strokeWidth: 4
         });
+        cup.on('click', function (e) {
+            var clickedIndex = cups.indexOf(e.target);
+        });
         cups.push(cup);
     }
+
     ballPosition = getRandomIndex();
-    var ball = new Kinetic.Circle({
+    ball = new Kinetic.Circle({
         x: getBallPositionByCupIndex(ballPosition),
         y: 250,
         radius: 15,
@@ -34,15 +49,15 @@ $(document).ready(function () {
         strokeWidth: 4
     });
 
+
+    cups[ballPosition].fill('#C3C3C3');
+
     // add objects to scene
+    layer.add(ball);
     _(cups).each(function (cup) {
         layer.add(cup);
     });
-    layer.add(ball);
-
     stage.add(layer);
-
-    swapCups(0, 1, function () { swapCups(1, 2); });
 });
 
 function swapCups(left, right, callback) {
@@ -72,16 +87,71 @@ function swapCups(left, right, callback) {
 }
 
 function getRandomNumber(min, max) {
-    return Math.floor((Math.random() * max) + min);
+    return Math.floor((Math.random() * (max + 1)) + min);
 }
 
 function getRandomIndex() {
     return getRandomNumber(0, cups.length);
 }
 
+function getRandomBetweenTwoNumbers(values) {
+    var random = getRandomNumber(0, 1);
+    if (random == 0)
+        return values[0];
+    return values[1];
+}
+
 function getBallPositionByCupIndex(cupIndex) {
     var selectedCup = cups[cupIndex];
     return selectedCup.getPosition().x + (selectedCup.getWidth() / 2);
+}
+
+function shuffle() {
+    var anim = new Kinetic.Animation(function (frame) {
+        _(cups).each(function (cup) {
+            var newY = cup.getPosition().y + 10;
+            cup.setY(newY);
+        });
+        var maxYWithHeight = getOffsetYWithHeight(ball);
+        var currentYWithHeight = getOffsetYWithHeight(cups[0]); 
+        if (currentYWithHeight >= maxYWithHeight) {
+            anim.stop();
+            hideBall();
+            swapRandomCups();
+        }
+    }, layer);
+    anim.start();
+}
+
+function getOffsetYWithHeight(kineticObject) {
+    return kineticObject.getPosition().y + kineticObject.getHeight()
+}
+
+function hideBall() {
+    ball.remove();
+    layer.draw();
+}
+
+function swapRandomCups() {
+    if (noOfSwapsMade >= maxNoOfSwaps)
+        return;
+    
+    noOfSwapsMade++;
+    var oldBallPosition = ballPosition;
+    var otherIndexes = getOtherIndexes(oldBallPosition);
+    ballPosition = getRandomBetweenTwoNumbers(otherIndexes);
+    if (oldBallPosition < ballPosition)
+        swapCups(oldBallPosition, ballPosition, swapRandomCups);
+    else
+        swapCups(ballPosition, oldBallPosition, swapRandomCups);
+}
+
+function getOtherIndexes(index) {
+    if (index == 0)
+        return [1, 2];
+    if (index == 1)
+        return [0, 2];
+    return [0, 1];
 }
 
 
